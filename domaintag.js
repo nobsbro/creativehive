3d142a974aeab2978d11925bbc49041721eb6fc3
nobsbro <82125507+nobsbro@users.noreply.github.com>
2021-04-08 02:25:15 +0300

    Add files via upload

---
+++ domaintag.js
@@ -0,0 +1,3 @@
+<script type="text/javascript">  
+void(0);
+</script>  
---
+++ googletag.js
@@ -0,0 +1,5 @@
+<script type="text/javascript">  
+  window.setTimeout(function () {
+    document.getElementById('lp-pom-box-70').style = 'display: block';
+    }, 1000);
+</script>  
